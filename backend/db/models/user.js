5bf45cc33ab6187cd272c94add39064bdb129658
KYLE GOOD <kyle.good@creditgenie.com>
2023-02-11 17:37:33 -0500

    Fixing user migration from rebase

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,45 +1,51 @@
 'use strict';
-const { Model, Validator } = require('sequelize');
 const bcrypt = require('bcryptjs');
+const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    validatePassword(password) {
-        return bcrypt.compareSync(password, this.hashedPassword.toString());
-    }
-    static getCurrentUserById(id) {
-        return User.scope("currentUser").findByPk(id);
-      }
     toSafeObject() {
       const { id, username, email } = this; // context will be the User instance
       return { id, username, email };
     }
+
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+
     static associate(models) {
       // define association here
     }
-    static async login({ credential, password }) {
-        const { Op } = require('sequelize');
-        const user = await User.scope('loginUser').findOne({
-          where: {
-            [Op.or]: {
-              username: credential,
-              email: credential
-            }
-          }
-        });
-        if (user && user.validatePassword(password)) {
-          return await User.scope('currentUser').findByPk(user.id);
-        }
-      }
-      static async signup({ username, email, password }) {
-        const hashedPassword = bcrypt.hashSync(password);
-        const user = await User.create({
-          username,
-          email,
-          hashedPassword
-        });
-        return await User.scope('currentUser').findByPk(user.id);
-      }
   };
 
   User.init(
@@ -54,6 +60,20 @@ module.exports = (sequelize, DataTypes) => {
               throw new Error("Cannot be an email.");
             }
           }
+        }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [20]
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [20]
         }
       },
       email: {
