13c818415ae2df3776a3f8912235ce9e423c823d
KYLE GOOD <kyle.good@creditgenie.com>
2023-02-12 00:55:12 -0500

    Fixing some code, refactoring, squashing a bug

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,8 @@
-// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
