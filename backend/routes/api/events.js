8b4e5985f37390cdde2624dd76a004adec2c74de
KYLE GOOD <kyle.good@creditgenie.com>
2023-02-25 15:30:10 -0500

    Adding a comment

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -9,6 +9,7 @@ router.use(restoreUser)
 
 // PM Get all Events
 // GH -- Need to add it for sanity checks
+//
 router.get('/events', async (req, res, next) => {
     try {
       const events = await Event.findAll({
