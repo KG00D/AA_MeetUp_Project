16ba555ef7e5ceaf0a5e303cf063eca0570918d7
KYLE GOOD <kyle.good@creditgenie.com>
2023-03-23 22:23:57 -0400

    nadda

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -10,10 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Event.hasMany(models.eventImage, {foreignKey: 'eventId'});
+      Event.belongsTo(models.Group, {foreignKey: 'groupId'});
       Event.belongsTo(models.Venue, {foreignKey: 'venueId'});
       Event.hasMany(models.Attendance, {foreignKey: 'eventId'});
-      Event.belongsTo(models.Group, {foreignKey: 'groupId'});
+      Event.hasMany(models.eventImage, {foreignKey: 'eventId'});
     }
   }
   Event.init({
