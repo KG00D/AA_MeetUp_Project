0962642aed2258b1432ba1f505570cad8c7718e6
KYLE GOOD <kyle.good@creditgenie.com>
2023-03-25 13:45:58 -0400

    changing my mind

--- backend/routes/api/group-images.js
+++ backend/routes/api/group-images.js
@@ -7,20 +7,26 @@ const router = express.Router();
 
 router.use(restoreUser)
 
-router.delete('/:imageId', requireAuth, async (req, res) => {
-  const imageId = Number(req.params.imageId);
-
-  try {
-    const groupImage = await GroupsImage.findByPk(imageId);
+router.delete('/:groupImageId', requireAuth, async (req, res) => {
+  const imageId = req.params.groupImageId;
+  const userId = req.user.id;
   
-    if (!groupImage) {
+  try {
+    const image = await GroupImage.findOne({ where: { id: imageId } });
+    if (!image) {
       return res.status(404).json({
         message: "Group image couldn't be found",
         statusCode: 404
       });
     }
-
-    await groupImage.destroy();
+    const group = await image.getGroup();
+    if (group.organizerId !== userId && group.coHostId !== userId) {
+      return res.status(403).json({
+        message: "Current user is not authorized to delete the image",
+        statusCode: 403
+      });
+    }
+    await image.destroy();
     return res.json({
       message: "Successfully deleted group image"
     });
