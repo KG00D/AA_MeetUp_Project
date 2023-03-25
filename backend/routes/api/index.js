313f1b08eede5f3d2088e3f71e3a69619403818e
KYLE GOOD <kyle.good@creditgenie.com>
2023-03-25 16:31:51 -0400

    Fixing JSON response for group creation

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,9 +9,6 @@ const eventImages = require('./event-images.js');
 const groupImagesrouter = require('./group-images.js')
 const { restoreUser } = require("../../utils/auth.js");
 
-// Connect restoreUser middleware to the API router
-// If current user session is valid, set req.user to the user in the database
-// If current user session is not valid, set req.user to null
 router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
