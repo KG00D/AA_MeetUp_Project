9868b886ebf3a080f5b955b3538dbb7982c49eb2
KYLE GOOD <kyle.good@creditgenie.com>
2023-03-25 14:33:16 -0400

    Saving spoit on refactor

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -7,30 +7,19 @@ const router = express.Router();
 
 router.use(restoreUser)
 
-// Delete an existing image for an Event
-router.delete('/:eventImageId', async (req, res) => {
+router.delete('/:eventImageId', requireAuth, async (req, res) => {
   const imageId = req.params.eventImageId;
-  const userId = req.user.id;
-  
+  console.log(imageId)
+  const eventImage = await eventImage.findByPk({ where: { id: imageId } });
+
   try {
-    const image = await groupImage.findOne({ where: { id: imageId } });
-    if (!image) {
-      return res.status(404).json({
-        message: "Event image couldn't be found",
-        statusCode: 404
+    if (eventImage) {
+      eventImage.destroy({
+        where: { id: imageId }
       });
-    }
-    const group = await image.getGroup();
-    if (group.organizerId !== userId && group.coHostId !== userId) {
-      return res.status(403).json({
-        message: "Current user is not authorized to delete the image",
-        statusCode: 403
-      });
-    }
-    await image.destroy();
-    return res.json({
-      message: "Successfully deleted group image"
-    });
+      return res.json({
+        message: "Successfully deleted group image"
+
   } catch (error) {
     console.error(error);
     throw error;
